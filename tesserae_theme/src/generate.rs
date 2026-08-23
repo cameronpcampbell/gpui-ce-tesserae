6eec0becb5e62322ad0f91a0c3224fb91792d861
Cameron Campbell <cameron.campbell325@gmail.com>
2026-08-23 14:45:57 +0100

    removed unused from generate.rs in tesserae_theme

--- tesserae_theme/src/generate.rs
+++ tesserae_theme/src/generate.rs
@@ -1,7 +1,5 @@
-use palette::rgb::channels::Rgba;
-
 use crate::{Theme, ThemeAccentsConfig, ThemeSetKind};
 
-pub fn generate_theme(config: &ThemeAccentsConfig, kind: ThemeSetKind) -> super::Theme {
+pub fn generate_theme(config: &ThemeAccentsConfig, kind: ThemeSetKind) -> Theme {
     unimplemented!()
 }
